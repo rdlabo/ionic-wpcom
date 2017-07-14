8e9aef1ac86597ba43243b2ee6f41358473a4728
榊原昌彦 <sakakibara@rdlabo.jp>
2017-07-14 21:09:55 +0900

    trim

--- src/providers/wordpress/wordpress.ts
+++ src/providers/wordpress/wordpress.ts
@@ -155,9 +155,10 @@ export class WordpressProvider {
         }
         params.title = <string>this.sanitizer.bypassSecurityTrustHtml(params.title);
         params.content = <string>this.sanitizer.bypassSecurityTrustHtml(params.content);
-        params.excerpt = <string>this.sanitizer.bypassSecurityTrustHtml(this.removeTag(params.excerpt));
         if(params.excerpt.length > 80){
-            params.excerpt = params.excerpt.substr(0, 80) + '…';
+            params.excerpt = <string>this.sanitizer.bypassSecurityTrustHtml(params.excerpt.substr(0, 80) + '…');
+        }else{
+            params.excerpt = <string>this.sanitizer.bypassSecurityTrustHtml(this.removeTag(params.excerpt));
         }
 
         return params;
