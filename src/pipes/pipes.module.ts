14a7bcbdfc89c693a4bc61a2554f708f1ae39922
榊原昌彦 <sakakibara@rdlabo.jp>
2017-07-14 17:45:45 +0900

    delete fileserver pipe

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { FileserverPipe } from './fileserver/fileserver';
 import { MydatePipe } from './mydate/mydate';
 import { KeyPipe } from './key/key';
 
 @NgModule({
   declarations: [
-    FileserverPipe,
     MydatePipe,
     KeyPipe
   ],
   exports: [
-    FileserverPipe,
     MydatePipe,
     KeyPipe
   ]
