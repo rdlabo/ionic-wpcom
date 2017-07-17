b458f8da35c28746e09652282fbdda8761a1d545
榊原昌彦 <sakakibara@rdlabo.jp>
2017-07-17 11:33:13 +0900

    add Ionic-strage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 import { StoreModule } from '@ngrx/store';
 import { reducer } from '../store/index';
 
@@ -23,6 +24,7 @@ import { SidebarComponentModule} from '../pages/template-parts/sidebar/sidebar.m
     HttpModule,
     IonicModule.forRoot(MyApp),
     StoreModule.provideStore(reducer),
+    IonicStorageModule.forRoot(),
     HeaderComponentModule,
     SidebarComponentModule
   ],
